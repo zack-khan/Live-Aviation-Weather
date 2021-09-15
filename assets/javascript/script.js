ac09f68b8825f1617d9e90ca6b73b0133a7ed5bc
Zachary Khan <zacharythekhan@gmail.com>
2021-09-15 17:03:13 -0500

    localstorage, additional wx data, bugfixes

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,56 +1,60 @@
-var searchBtn = $("#search-btn");
-var titleEl = $("#searched-city")
-var temp = $('#temp');
-var wind = $('#wind');
-var humidity = $('#humidity');
-var fltCond = $('#flt-cond');
-var searched = $("#text-in");
-var forecastBoxes = $("#forecast-boxes")
-var station = 'KATL';
-var historyList = $('#history');
+const searchBtn = $("#search-btn");
+const titleEl = $("#searched-city")
+const temp = $('#temp');
+const wind = $('#wind');
+const humidity = $('#humidity');
+const visibility = $('#visibility');
+const altSetting = $('#altSetting');
+const fltCond = $('#flt-cond');
+const searched = $("#text-in");
+const forecastBoxes = $("#forecast-boxes")
+let station = '';
+const historyList = $('#history');
+let stations = [];
 
+// localStorage.setItem('Airports', JSON.stringify(stations));
 
-function getWeather () {
-    fetch("https://api.checkwx.com/metar/" + station + "/decoded/?x-api-key=add95b072d72452b81e4726333")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            console.log(data.data[0]);
-            wxData = data.data[0];
-            populateWeather(wxData);
-            getForecast();
-        });
+async function getWeather () {
+    let res = await fetch(`https://api.checkwx.com/metar/${station}/decoded/?x-api-key=add95b072d72452b81e4726333`);
+    let wxData = await res.json();
+    wxData = wxData.data[0];
+
+    populateWeather(wxData);
+    getForecast();
 }
 
-function getForecast () {
-    fetch("https://api.checkwx.com/taf/" + station + "/decoded?x-api-key=add95b072d72452b81e4726333")
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            console.log(data.data[0]);
-            fxData = data.data[0];
-            populateForecast(fxData)
-        });
+async function getForecast () {
+
+    let res = await fetch(`https://api.checkwx.com/taf/${station}/decoded?x-api-key=add95b072d72452b81e4726333`);
+
+    let fxData = await res.json();
+    fxData = fxData.data[0];
+
+    // fxData ? populateForecast(fxData) : clearForecast()
+
+    populateForecast(fxData);
 }
 
 
 function populateWeather(wxData) {
-    titleEl.text(wxData.station.name + " (" + wxData.icao + ")");
-    temp.text("Temperature: " + wxData.temperature.fahrenheit + "°F");
-    wind.text("Wind: Heading " + wxData.wind.degrees + "° at " + wxData.wind.speed_kts + " kts");
-    humidity.text("Humidity: " + wxData.humidity.percent + "%");
-    fltCond.text("Flight Category: " + wxData.flight_category);
-    fltCond.removeClass();
-    fltCond.addClass(wxData.flight_category);
+    if (wxData) {
+        titleEl.text(wxData.station.name + " (" + wxData.icao + ")");
+        temp.text("Temperature: " + wxData.temperature.fahrenheit + "°F");
+        wind.text("Wind: Heading " + wxData.wind.degrees + "° at " + wxData.wind.speed_kts + " kts");
+        humidity.text("Humidity: " + wxData.humidity.percent + "%");
+        visibility.text(`Visibility: ${wxData.visibility.miles} mi`);
+        altSetting.text(`Altimeter Setting: ${wxData.barometer.hg}`);
+        fltCond.text("Flight Category: " + wxData.flight_category);
+        fltCond.removeClass();
+        fltCond.addClass(wxData.flight_category);
+    } else {
+        titleEl.text(`No Live Data Found!`)
+    }
 }
 
 function populateForecast(fxData) {
     for (i = 0; i < 5; i++) {
-        if (fxData.forecast[i]) {
+        if (fxData) {
             if (fxData.forecast[i].timestamp.from) {
                 forecastBoxes.children().eq(i).children().eq(0).children().eq(0).text(fxData.forecast[i].timestamp.from + " to " + fxData.forecast[i].timestamp.to);
             }
@@ -88,26 +92,62 @@ function populateForecast(fxData) {
 }
 
 function addToHistory () {
-    historyList.prepend('<button class="text-center text-xl bg-gray-400 rounded-sm w-full hover:bg-gray-500 mb-1" type="button"></button>');
-    historyList.children().first().text(station);
-    historyList.children().on("click", function() {
-        console.log(this);
-        var buttonClicked = $(this);
-        station = buttonClicked.text();
-        console.log(station);
-        searched.val(station);
-        getWeather();
-    });
+    let exists = false;
+    stations = JSON.parse(localStorage.getItem('Airports'));
+    // console.log(stations);
+    if (stations) { 
+        stations.forEach(stationListItem => station == stationListItem ? exists = true : null); // Check if entered station already exists in history list
+    } else {
+        exists = false;
+        stations = [];
+    }
+
+    if (!exists) {
+        stations.push(station);
+        localStorage.setItem('Airports', JSON.stringify(stations));
+
+        historyList.prepend('<button class="text-center text-xl bg-gray-400 rounded-sm w-full hover:bg-gray-500 mb-1" type="button"></button>');
+        historyList.children().first().text(station);
+        historyList.children().on("click", function() {
+            // console.log(this);
+            let buttonClicked = $(this);
+            station = buttonClicked.text();
+
+            // console.log(station);
+            searched.val(station); // Sets input text to clicked button
+            getWeather();
+        });
+    }
+}
+
+window.onload = (e) => {
+    stations = JSON.parse(localStorage.getItem("Airports"));
+
+    if (stations) {
+        stations.forEach(stationInList => {
+            historyList.prepend('<button class="text-center text-xl bg-gray-400 rounded-sm w-full hover:bg-gray-500 mb-1" type="button"></button>');
+            historyList.children().first().text(stationInList);
+            historyList.children().on("click", function() {
+                // console.log(this);
+                let buttonClicked = $(this);
+                station = buttonClicked.text();
+
+                // console.log(station);
+                searched.val(station); // Sets input text to clicked button
+                getWeather();
+            });
+        })
+    }
 }
 
 searchBtn.on("click", function() {
-    station = searched.val();
+    station = searched.val().trim();
     station = station.toUpperCase();
-    console.log(station);
-    addToHistory();
-    getWeather();
+
+    if (station.length !== 4) {
+        // Do nothing
+    } else {
+        addToHistory();
+        getWeather();
+    }
 });
-
-
-
-getWeather();
